9be16643c620836ea36b9ea1cdbd72096819297f
FiladAmahasl <114080314+FiladAmahasl@users.noreply.github.com>
2023-05-31 16:50:40 +0300

    5.c

--- HW4/5.c
+++ HW4/5.c
@@ -2,7 +2,7 @@
 
 int main(){
     int numb;
-    double sum = 0, allnumb = 0;
+    double sum = 0, allnumb = 0; //Объявление переменной sum с типом double (сумма введенных чисел) и переменной allnumb с типом double (количество введенных чисел).
     scanf("%d", &numb);
     while (numb != 0) //Проверка пока значение переменной не равно 0
     {
